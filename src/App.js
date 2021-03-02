2e920893bed173f1f26e5d6163b14848ff77af27
Ivan Mercedes <ivanmercede@gmail.com>
2021-03-02 09:49:06 -0400

    fix .gitignore duplicate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine'
+
+import ChatFeed from './components/ChatFeed'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return ( 
+        <ChatEngine
 
+            height="100vh"
+            projectID="4ee30f47-b4d1-4c96-88a4-1c11224c314d"
+            userName="IvanMercedes"
+            userSecret="123456789"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        
+        />
+     );
+}
+ 
 export default App;
